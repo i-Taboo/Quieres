3cebadab369d21b045ae6168f4f99f7a820cff11
i-Taboo <82161706+i-Taboo@users.noreply.github.com>
2021-04-10 13:20:28 -0400

    Add files via upload

---
+++ js/entidades.js
@@ -0,0 +1,39 @@
+function heh(e) {
+    document.getElementById("No1").classList.add("No");
+    document.getElementById("No1").classList.remove("no1");
+    document.getElementById("nu").classList.remove("No");
+    document.getElementById("nu").classList.add("no1");
+}
+function heh1(e) {
+    document.getElementById("No1").classList.remove("No");
+    document.getElementById("No1").classList.add("no1");
+    document.getElementById("no2").classList.remove("no2");
+    document.getElementById("no2").classList.add("No");
+}
+function heh2(e) {
+    document.getElementById("no2").classList.remove("No");
+    document.getElementById("no2").classList.add("no2");
+    document.getElementById("no3").classList.remove("no3");
+    document.getElementById("no3").classList.add("No");
+}
+function heh3(e) {
+    document.getElementById("no3").classList.remove("No");
+    document.getElementById("no3").classList.add("no3");
+    document.getElementById("no4").classList.remove("no4");
+    document.getElementById("no4").classList.add("No");
+}
+
+function heh4(e) {
+    document.getElementById("no4").classList.remove("No");
+    document.getElementById("no4").classList.add("no4");
+    document.getElementById("no5").classList.remove("no5");
+    document.getElementById("no5").classList.add("No");
+}
+
+function fin5(e){
+    document.getElementById("no5").classList.remove("No");
+    document.getElementById("no5").classList.add("no5");
+    document.getElementById("No1").classList.add("No");
+    document.getElementById("No1").classList.remove("no1");
+}
+
